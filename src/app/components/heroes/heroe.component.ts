b126d790e990fd8552c87d2ccd274f93675c1059
Alejandro Jimenez <lagaleria17@gmail.com>
2018-10-30 17:55:44 -0300

    fruta

--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { HeroesService } from '../../services/heroes.service';
 
 import {Heroe} from "../../interfaces/heroe.interface";
@@ -17,8 +17,9 @@ export class HeroeComponent implements OnInit {
         casa: 'Marvel'
     }
 
-    constructor( private _heroesService:HeroesService,
-                 private router:Router) {
+    constructor( private _heroesService: HeroesService,
+                 private router: Router,
+                 private _route: ActivatedRoute ) {
     }
 
     ngOnInit() {
@@ -26,11 +27,20 @@ export class HeroeComponent implements OnInit {
 
     guardar(){
         console.log(this.heroe);
-        this._heroesService.nuevoHeroe( this.heroe )
-            .subscribe( data=>{
-                    this.router.navigate(['/heroe', data.name])
-            },
-            error => console.error(error));
+        let key: string = this._route.params.value['id'];
+        if( key === 'nuevo') {
+            this._heroesService.nuevoHeroe( this.heroe )
+                .subscribe( data => {
+                        this.router.navigate(['/heroe', data.name]);
+                    },
+                    error => console.error(error));
+        }else{
+            this._heroesService.actualizarHeroe( this.heroe, key )
+                .subscribe( data => {
+                        console.log(data);
+                    },
+                    error => console.error(error));
+        }
     }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -9,9 +9,10 @@ import { map } from 'rxjs/operators';
 export class HeroesService {
 
   firebaseUrl = "https://heroesapp-4d907.firebaseio.com/heroes.json";
+  firebaseHeroeUrl = "https://heroesapp-4d907.firebaseio.com/heroes/";
   constructor( private http:HttpClient ) { }
 
-  nuevoHeroe( heroe:Heroe, ){
+  nuevoHeroe( heroe:Heroe ){
       let body = JSON.stringify( heroe );
       let headers = new HttpHeaders({
         "Content-Type": "application/json"
@@ -22,7 +23,23 @@ export class HeroesService {
             return res;
           })
       );
+  }
 
+  actualizarHeroe(heroe: Heroe, key$: string ){
+      let body = JSON.stringify( heroe );
+      let headers = new HttpHeaders({
+          "Content-Type": "application/json"
+      });
+      let _url = `${this.firebaseHeroeUrl}${key$}.json `;
+      return this.http.put( _url, body, { headers }).pipe(
+          map(res =>{
+              return res;
+          })
+      );
+  }
 
+  getHeroe( key$:string ){
+      let url = `${ this.heroeURL }/${key$}.json`;
+      return this.https.get( url )
   }
 }
